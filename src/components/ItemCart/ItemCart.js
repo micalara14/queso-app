19dadecdb6514ffa02024a3faa1cf02b9a74590e
Micaela Mendoza <micaelamendoza1998@gmail.com>
2022-06-27 22:55:21 -0300

    form estilado

--- src/components/ItemCart/ItemCart.js
+++ src/components/ItemCart/ItemCart.js
@@ -20,7 +20,7 @@ const ItemCart = () => {
             {cart.length < 1 ? <div className="carrito-vacio" > <p className="titulo-carrito" >No hay productos en el carrito</p> <Link to="/"> Vovler a la tienda </Link> </div> : <p className="titulo-carrito" >Carrito de compras</p>}
             {cart.map(prod => <div key={prod.id}> <p>{prod.name}</p> <p>Cantidad: {prod.quantity}</p> <p>Subtotal: ${prod.quantity*prod.price}</p>  <Button onClick={() => removeItem(prod.id)} variant="warning"> X </Button> </div>)}
         </div>
-            {cart.length > 0 ?  <div> <p className="total-compra" >Total de su compra: ${total}</p> <Button onClick={() => deleteAll()} variant="warning"> Vaciar carrito </Button> {/* <Button onClick={createOrder} variant="warning"> Crear orden </Button> */} <ItemForm/> </div> : null}
+            {cart.length > 0 ?  <div> <p className="total-compra" >Total de su compra: ${total}</p> <Button onClick={() => deleteAll()} variant="warning"> Vaciar carrito </Button> <ItemForm/> </div> : null}
         </div>
         
         
--- src/components/ItemForm/ItemForm.js
+++ src/components/ItemForm/ItemForm.js
@@ -1,14 +1,16 @@
+//useState useContext
 import { useState } from 'react';
-
+import { useContext } from "react"
+import CartContext from "../../context/CarContext"
 //Firestore
 import { addDoc, collection} from "firebase/firestore"
 import { db } from '../../services/firebase';
 // Bootstrap
 import { Button } from 'react-bootstrap';
-//context
-import { useContext } from "react"
-import { Link } from "react-router-dom"
-import CartContext from "../../context/CarContext"
+//CSS
+import "./ItemForm.css"
+//Sweet Alert
+import swal from 'sweetalert';
 
 const ItemForm = () => {
     const [buyer, setBuyer] = useState({
@@ -31,25 +33,30 @@ const ItemForm = () => {
         const collectionRef = collection(db, "orders")
 
         addDoc(collectionRef, order).then(({id}) => {
-        alert(`Se creo la orden con el id ${id}`)
-        console.log(`Se creo la orden con el id ${id}`)
+        swal(`Se creo la orden con el id ${id}.
+        Gracias por su compra! Nos pondremos en contacto.`)
         })
         
     }
 
 
     return (
-        <div>
+        <div className='div-form'>
             <h3>Complete el formulario para finalizar la compra:</h3>
             <form>
-            <input value={buyer.name} placeholder="Nombre" onChange={(e) => setBuyer({...buyer, name: e.target.value})} />
-            <input value={buyer.email} placeholder="Email" onChange={(e) => setBuyer({...buyer, email: e.target.value})} />
-            <input value={buyer.phone} placeholder="Teléfono" onChange={(e) => setBuyer({...buyer, phone: e.target.value})} />
-            <input value={buyer.adress} placeholder="Dirección" onChange={(e) => setBuyer({...buyer, adress: e.target.value})} />
-            <input value={buyer.comment} placeholder="Comentarios" onChange={(e) => setBuyer({...buyer, comment: e.target.value})} />
+            <label for="name">Ingrese su nombre</label>
+            <input id="name" value={buyer.name} placeholder="Nombre" onChange={(e) => setBuyer({...buyer, name: e.target.value})} />
+            <label for="email">Ingrese su email</label>
+            <input id="email" value={buyer.email} placeholder="Email" onChange={(e) => setBuyer({...buyer, email: e.target.value})} />
+            <label for="phone">Ingrese su teléfono</label>
+            <input id="phone" value={buyer.phone} placeholder="Teléfono" onChange={(e) => setBuyer({...buyer, phone: e.target.value})} />
+            <label for="adress">Ingrese su dirección completa</label>
+            <input id="adress" value={buyer.adress} placeholder="Dirección" onChange={(e) => setBuyer({...buyer, adress: e.target.value})} />
+            <label for="comment">Comentarios o notas a tener en cuenta</label>
+            <input id="comment" value={buyer.comment} placeholder="Comentarios" onChange={(e) => setBuyer({...buyer, comment: e.target.value})} />
 
             </form>
-            {buyer.name != "" && buyer.email != "" && buyer.phone != "" && buyer.adress != "" && buyer.comment != "" ? <Button onClick={createOrder} variant="warning"> Crear orden </Button> : null}
+            {buyer.name != "" && buyer.email != "" && buyer.phone != "" && buyer.adress != "" && buyer.comment != "" ? <Button className="btn-comprar" onClick={createOrder} variant="warning"> Crear orden </Button> : null}
             
         </div>
     )
